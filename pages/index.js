dabe983468a9d6ce8c82169796e62d2192cfae6a
Anselem <anselemodims@gmail.com>
2022-09-28 11:46:38 +0100

    Data fetching and dynamic pages

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout, {siteTitle} from '../components/Layout';
 import utilStyles from '../components/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const postsData = getSortedPostsData();
+  return {
+    props: {
+      postsData,
+    }
+  }
+}
+
+export default function Home({ postsData }) {
   return (
     <Layout home>
       <Head>
@@ -15,6 +25,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {postsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
